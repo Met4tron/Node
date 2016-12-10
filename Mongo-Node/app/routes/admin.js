cf7648a34be2b588aa9ef0496636e97b5d1ac1f0
Met4tron <yuurig@gmail.com>
2016-12-10 14:32:31 -0200

    Implementação de Classes Javascript ( ES5)

--- Mongo-Node/app/routes/admin.js
+++ Mongo-Node/app/routes/admin.js
@@ -1,18 +1,18 @@
-module.exports = function(app) {
-  app.get('/form_noticia', (req, res) =>{
+module.exports = function (app) {
+  app.get('/form_noticia', (req, res) => {
     res.render('admin/form_add_noticia');
   });
-  app.post('/noticias/salvar', function(req, res){
+  app.post('/noticias/salvar', function (req, res) {
     var noticia = req.body;
-    
-    var connection = app.config.dbConnection();
-    var noticiasModel = app.app.models.noticiasModel;
 
-    noticiasModel.salvarNoticia(noticia, connection, function(error, result){
+    var connection = app.config.dbConnection();
+    var noticiasModel = new app.app.models.NoticiasDAO(connection);
+
+    noticiasModel.salvarNoticia(noticia, function (error, result) {
       res.redirect('/noticias');
     })
-    
-    
+
+
     //Conexao
     //Model
     //salvarNoticia
--- Mongo-Node/app/routes/noticia.js
+++ Mongo-Node/app/routes/noticia.js
@@ -3,9 +3,9 @@ module.exports = function(app) {
     app.get('/noticia', function(req, res) {
 
         var connection = app.config.dbConnection();
-        var modelNoticia = app.app.models.noticiasModel();
+        var modelNoticia = new app.app.models.NoticiasDAO(connection);
 
-        modelNoticia.getNoticia(connection, function(err, result){
+        modelNoticia.getNoticia(function(err, result){
           if(err) throw err
           res.render('noticias/noticia', {noticia: result});
         })
